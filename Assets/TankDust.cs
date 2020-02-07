4d5efe0d99707349b4efc74d51ee664ecc730bbe
vivogd <aheilweil@gmail.com>
2020-02-07 11:18:06 +0200

    Modified Tank Code
    Digging capabilities
    Particle System

---
+++ Assets/TankDust.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Complete {
+    public class TankDust : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+        public void Beep() {
+            Debug.Log("BEEEEEP");
+        }
+    }
+
+}
---
+++ Assets/_Completed-Assets/Scripts/Tank/TankHide.cs
@@ -0,0 +1,78 @@
+﻿using Complete;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Complete {
+
+    public class TankHide : MonoBehaviour {
+
+        public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+        public Animator anim;                      // Spin animator
+        public ParticleSystem tornadoPrefab;
+        public float damagePerSecond; 
+        public bool isHidden;
+
+        private TankHealth tankHealth;
+        private string m_HideButton;                // The input axis that is used for launching shells.
+        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+      
+
+
+
+        private void Awake() {
+
+            m_Rigidbody = GetComponent<Rigidbody>();
+            
+        }
+
+        void Start() {
+            tankHealth = GetComponent<TankHealth>();
+            // tankHealth = m_Rigidbody.GetComponent<TankHealth>();
+            Debug.Log("tankHealth " + tankHealth);
+            isHidden = false;
+            m_HideButton = "Hide" + m_PlayerNumber;
+        }
+
+        void Update() {
+            if (Input.GetButtonDown(m_HideButton)) {
+                if (!isHidden) {
+                    HideMe();
+
+                }
+
+                else ShowMe();
+            }
+
+
+            if (isHidden) {
+                tankHealth.TakeDamage(damagePerSecond);
+            }
+        }
+
+        void HideMe() {
+            anim.SetTrigger("hideMe");
+            isHidden = true;
+            ParticleSystem tornado = Instantiate(tornadoPrefab, transform.position, transform.rotation);
+            tornado.Play();
+            ParticleSystem.MainModule mainModule = tornado.main;
+            Destroy(tornado.gameObject, mainModule.duration);
+        }
+
+        void ShowMe() {
+            anim.SetTrigger("showMe");
+            isHidden = false;
+            ParticleSystem tornado = Instantiate(tornadoPrefab, transform.position, transform.rotation);
+            tornado.Play();
+            ParticleSystem.MainModule mainModule = tornado.main;
+            Destroy(tornado.gameObject, mainModule.duration);
+        }
+
+        void IsHiddenEvent() {
+            Debug.Log("IS HIDDEN EVENT");
+        }
+
+    }
+
+}
